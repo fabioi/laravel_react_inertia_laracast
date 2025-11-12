d5a9785859ddc3ae83165e6caac02b813940b557
Fabio Ingrasciotta <fabio.ingrasciotta@gmail.com>
2025-11-12 18:30:21 +0100

    Add `UserResource` and update `PuppyResource` to use it for consistent relationship formatting

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -5,18 +5,13 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class PuppyResource extends JsonResource
+class UserResource extends JsonResource
 {
     public function toArray(Request $request)
     {
         return [
             'id' => $this->id,
             'name' => $this->name,
-            'trait' => $this->trait,
-            'imageUrl' => $this->image_url,
-            // Make is used when we load one relationship
-            'user' => UserResource::make($this->whenLoaded('user')),
-
         ];
     }
 }
