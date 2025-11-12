92ca5a7aa5e7aad726f5529a8c6acf28f6e6076e
Fabio Ingrasciotta <fabio.ingrasciotta@gmail.com>
2025-11-12 15:35:07 +0100

    Add `Puppy` model, migration, factory, and seeder

---
+++ app/Models/Puppy.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Puppy extends Model
+{
+    /** @use HasFactory<\Database\Factories\PuppyFactory> */
+    use HasFactory;
+}
