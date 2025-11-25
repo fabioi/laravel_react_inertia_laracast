9e2abf2fb26e44b436b16cdba0a50a361a2f07f2
Fabio <fabio.ingrasciotta@gmail.com>
2025-11-25 14:44:53 +0100

    enabled short list

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -30,7 +30,7 @@ function Main({ inertiaPuppies: inertiaPuppies }: { inertiaPuppies: Puppy[] }) {
         <main>
             <div className="mt-24 grid gap-8 sm:grid-cols-2">
                 <Search searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
-                {auth.user && <Shortlist puppies={puppies} setPuppies={setPuppies} />}
+                {auth.user && <Shortlist puppies={inertiaPuppies} setPuppies={setPuppies} />}
             </div>
             <PuppiesList puppies={inertiaPuppies} searchQuery={searchQuery} />
             <NewPuppyForm puppies={inertiaPuppies} setPuppies={setPuppies} />
