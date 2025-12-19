24369ceec8d9ee101314fc1d1ddb9ee3688d3529
Fabio <fabio.ingrasciotta@gmail.com>
2025-12-19 12:09:00 +0100

    removed main component and moved to main function, to simply and reduce prop drilling

--- resources/js/pages/puppies/index.tsx
+++ resources/js/pages/puppies/index.tsx
@@ -11,28 +11,23 @@ import { usePage } from '@inertiajs/react';
 import { useRef } from 'react';
 
 export default function App({ puppies,likedPuppies, filters }: { puppies: PaginatedResponse<Puppy>; likedPuppies: Puppy[]; filters: Filters }) {
+    const { auth } = usePage<SharedData>().props;
+    const mainRef = useRef<HTMLElement>(null);
+
     return (
         <PageWrapper>
             <Container>
                 <Header />
-                <Main paginatedPuppies={puppies} likedPuppies={likedPuppies} filters={filters} />
+                <main ref={mainRef} className="scroll-mt-6">
+                    <div className="mt-24 grid gap-8 sm:grid-cols-2">
+                        <Search filters={filters} />
+                        {auth.user && <Shortlist puppies={likedPuppies} />}
+                    </div>
+                    <PuppiesList puppies={puppies} />
+                    {auth.user && <NewPuppyForm mainRef={mainRef} />}
+                </main>
             </Container>
         </PageWrapper>
     );
 }
 
-function Main({ paginatedPuppies, likedPuppies,  filters }: { paginatedPuppies: PaginatedResponse<Puppy>; likedPuppies: Puppy[], filters: Filters }) {
-    const { auth } = usePage<SharedData>().props;
-    const mainRef = useRef<HTMLElement>(null);
-
-    return (
-        <main ref={mainRef} className="scroll-mt-6">
-            <div className="mt-24 grid gap-8 sm:grid-cols-2">
-                <Search filters={filters} />
-                {auth.user && <Shortlist puppies={likedPuppies} />}
-            </div>
-            <PuppiesList puppies={paginatedPuppies} />
-            {auth.user && <NewPuppyForm mainRef={mainRef} />}
-        </main>
-    );
-}
